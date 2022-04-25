1db21363bc0f85303320bea8b15f9c80f1619611
Matheus Simone <matheus@pop-os.localdomain>
2022-04-25 12:43:01 -0300

    new embedding

--- qrcode_flutter/android/src/main/kotlin/br/app/qrcode/qrcode_flutter/QrcodeFlutterPlugin.kt
+++ qrcode_flutter/android/src/main/kotlin/br/app/qrcode/qrcode_flutter/QrcodeFlutterPlugin.kt
@@ -1,9 +1,10 @@
 package br.app.qrcode.qrcode_flutter
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** QrcodeFlutterPlugin */
-class QrcodeFlutterPlugin {
+class QrcodeFlutterPlugin : FlutterPlugin {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
